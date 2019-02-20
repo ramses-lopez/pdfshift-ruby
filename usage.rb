73023c1c1a805bb75fe0439f1d9f33b4d1b7df3e
Ramses Lopez <ramses.lopezs@gmail.com>
2019-02-20 18:59:46 -0500

    removed unused code

--- usage.rb
+++ usage.rb
@@ -1,20 +1,12 @@
 require 'uri'
 require 'net/http'
-require 'openssl'
 require 'json' # for hash to_json conversion
 
 url = URI("https://api.pdfshift.io/v2/convert/")
+data = { "source" => "https://www.example.com", "sandbox" => true }
+headers = {"Content-Type" => "application/json"}
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-data = { "source" => "https://www.example.com", "sandbox" => false }
-
-response = Net::HTTP.post(url,
-  data.to_json,
-  "Content-Type" => "application/json"
-)
+response = Net::HTTP.post(url, data.to_json, headers )
 
 # Ensure that response.code == 200
 if response.code == '200'
