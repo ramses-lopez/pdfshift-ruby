7addf19367ea7e34767fbb81b37beda9408355d5
Ramses Lopez <ramses.lopezs@gmail.com>
2019-02-20 22:40:49 -0500

    Examples to be tested

---
+++ notes.md
@@ -0,0 +1,9 @@
+# TO Test
+- watermark
+- with_css_string
+- with_css_url
+
+# Notes
+- the custom header flag is http_header, not headers as shown on the python guide
+- Also,mention that you could access the API without sending the basic auth
+- Image in watermark example cannot be accessed
---
+++ with_css_url.rb
@@ -0,0 +1,25 @@
+require 'uri'
+require 'net/https'
+require 'json' # for hash to_json conversion
+
+uri = URI("https://api.pdfshift.io/v2/convert/")
+data = {"source" => "https://www.example.com",
+  "css" => "https://www.example.com/public/css/print.css",
+  "sandbox" => true}
+
+Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.body = data.to_json
+  request["Content-Type"] = "application/json"
+  request.basic_auth '3536598326b04512a4ae320e8d2c5f34', ''
+
+  response = http.request(request)
+
+  if response.code == '200'
+    # Since Ruby 1.9.1 only:
+    File.binwrite("result.pdf", response.body)
+  else
+    # Handle other codes here
+    puts "#{response.code} #{response.body}"
+  end
+end
