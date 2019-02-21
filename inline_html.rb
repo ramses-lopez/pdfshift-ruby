03bb9dc294593aef03550a27987bc3baa76c0136
Ramses Lopez <ramses.lopezs@gmail.com>
2019-02-20 21:15:07 -0500

    Added inline html example

---
+++ inline_html.rb
@@ -0,0 +1,23 @@
+require 'uri'
+require 'net/https'
+require 'json' # for hash to_json conversion
+
+file = File.read("invoice.html")
+uri = URI("https://api.pdfshift.io/v2/convert/")
+data = {"source" => file, "sandbox" => true}
+
+Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.body = data.to_json
+  request["Content-Type"] = "application/json"
+  request.basic_auth '3536598326b04512a4ae320e8d2c5f34', ''
+
+  response = http.request(request)
+
+  if response.code == '200'
+    # Since Ruby 1.9.1 only:
+    File.binwrite("result.pdf", response.body)
+  else
+    # Handle other codes here
+  end
+end
